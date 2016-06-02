b6b9bf01a324e86053f36bdef72e9130d8bcc8ff
sebcorrigan <sc1022@york.ac.uk>
2016-06-02 08:47:38 +0100

    commit before pulling main commented

--- src/userInterface/StackPaneUpdater.java
+++ src/userInterface/StackPaneUpdater.java
@@ -1,32 +1,41 @@
 package userInterface;
 
 
-import java.util.HashMap;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
+import java.util.HashMap;
+
 
 /**
- * Controller that loads, adds and removes screens from
- * a Stack Pane.
- * 
- * @author Seb
+ * A screen controller that allows switching between in app screens.
+ * This class extends a <code>StackPane</code> which is used as the parent of all in app screens.
+ * Includes methods to load nodes into a <code>HashMap</code> and access these nodes by their <code>key</code>.
+ * This allows a screen to be removed and replaced by the next screen using the {@link #setScreen(String) setScreen(nextScreenID)}
+ * method that belongs to this class.
  *
+ * <p> <STRONG> Developed by </STRONG> <p>
+ * Sebastien Corrigan
+ * <p> <STRONG> Tested by </STRONG> <p>
+ * Sebastien Corrigan
+ * <p> <STRONG> Developed for </STRONG> <p>
+ * BOSS
+ * @author Sebastien Corrigan
  */
 public class StackPaneUpdater extends StackPane {
+	//================================================================================
+	// Fields
+	//================================================================================
 	private final double screenWidth;
 	private final double screenHeight;
 	
 	private DietMenu dietMenu;
+
 	/**
-	 * Hashmap is a map of all of the screens
-	 * String is the node's ID
-	 * Node is
+	 * Reference to the main application.
 	 */
-	// Reference to the main application.
 	private Main mainApp;
 
 	private DietPlanner dietPlanner;
@@ -34,68 +43,87 @@ public class StackPaneUpdater extends StackPane {
 	private CreateCharacter createCharacter;
 	private SocialMenu socialMenu;
 
+	/**
+	 * Holds a map of in app screens.
+	 * Type of Key maintained by this map: String.
+	 * Type of mapped values: Nodes.
+	 */
 	private HashMap<String, Node> screenMap = new HashMap<>();
 
-	public void setMainApp(Main mainApp) {
-		this.mainApp = mainApp;
-	}
+	//================================================================================
+	// Constructor
+	//================================================================================
 
+	/**
+	 * Constructor for this class
+	 * @param screenWidth dimensions used to give screen width and height to the extended <code>StackPane</code>
+	 * @param screenHeight
+     */
 	public StackPaneUpdater(double screenWidth, double screenHeight) {
 		// create the stack pane
 		super();
-		// screen dimensions will be used to build screens
+		// screen dimensions will be used for constructors of built screens
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
-
-
 	}
 
+
+
+	//================================================================================
+	// Members
+	//================================================================================
+	/**
+	 * Uses the {@link java.util.HashMap#put Hashmap.put()} method to associate a screen's node
+	 * with a screenID and load the screen into <code>screenMap</code> so that it can later be displayed
+	 * <code>put</code> returns the previous value at the associated screenID and since this return
+	 * is not assigned, the old screen nodes become elligible for garbage collection.
+	 *
+	 * @param screenID associated string key
+	 * @param screen screen node value
+     */
 	public void addScreen(String screenID, Node screen) {
-		// put is a hashmap method that associates a string
-		// with a value
-		// in this case the string is the screen name
-		// the value is the sceen's nodes
 		screenMap.put(screenID, screen);
-		// .put returns the previous if given duplicate ID
-		// since the return is not assigned, this method can be
-		// used to update a screen in the hashmap
-		// the old screen will be garbage collected
-
 	}
-
-	public Node getScreen(String screenID) {
-		return screenMap.get(screenID);
-	}
-
+	/**
+	 * Uses {@link javafx.fxml.FXMLLoader#load() FXMLLoader.load()} to build the screen nodes expressed
+	 * by an FXML file to then add it to the <code>screenMap</code>
+	 *
+	 * @param screenID the ID to be associated with the loaded fxml file's nodes
+	 * @param resource relative path to the fxml file to be loaded
+     * @return
+     */
 	public boolean loadFXMLScreen(String screenID, String resource) {
 		try {
-
 			FXMLLoader myLoader = new FXMLLoader(getClass().getResource(resource));
-
 			// get node
 			Parent parent = (Parent) myLoader.load();
-			// get the controller
+			// get the controller and use interface casting to be able to invoke the
+			// .setScreen and .setMainApp methods so that it can be Controllable
 			Controllable fxmlController = (Controllable) myLoader.getController();
 			// tell the controller that this is the parent node
 			fxmlController.setScreenParent(this);
 			fxmlController.setMainApp(mainApp);
-			// add the node to hashmap
+			// add the node screenMap
 			addScreen(screenID, parent);
 			return true;
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("loadFXMLScreen: fxml file could not be loaded");
-			System.out.println(resource);
 			return false;
 		}
 	}
 
-	public void loadJavaWrittenScreen(String screenID, Node javaScreen) {
+	/**
+	 * This method takes the parent node of a screen that has already been built from
+	 * the instantiation of its object. This method then associates the node with the
+	 * <code>screenID</code> <code>Key</code> and uses <code>addScreen</code> to load it in the <code>screenMap</code>
+	 * @param screenID associated string key
+	 * @param node screen node value
+     */
+	public void loadJavaWrittenScreen(String screenID, Node node) {
 		// get top level node
-		Parent parent = (Parent) javaScreen;
+		Parent parent = (Parent) node;
 		if (parent != null) {
 			// allow use of setScreenParent and setMainApp
-			Controllable javaScreenClass = (Controllable) javaScreen;
+			Controllable javaScreenClass = (Controllable) node;
 			javaScreenClass.setScreenParent(this);
 			javaScreenClass.setMainApp(mainApp);
 			addScreen(screenID, parent);
@@ -109,10 +137,10 @@ public class StackPaneUpdater extends StackPane {
 	}
 
 	/**
-	 * Show the new screen with a transition
+	 * Shows the new screen
 	 *
 	 * @param screenID the new screen to be shown
-	 * @return
+	 * @return true/false if screen set
 	 */
 	public boolean setScreen(final String screenID) {
 		/** Ensure new screen ID is valid by checking hashmap **/
@@ -133,48 +161,69 @@ public class StackPaneUpdater extends StackPane {
 
 	}
 
-	public HashMap<String, Node> getScreenMap() {
-		return screenMap;
-	}
-
-
+	/**
+	 * Removes the top layer of <code>this</code> and places the screen associated with the parameter
+	 * @param screenID key value for the screen to be swapped to
+     */
 	public void removeTopLayerThenAddNewTopLayer(String screenID) {
-		if (!getChildren().isEmpty()) { // if the node is not empty
-			// remove the node at index 0 ie the top layer
+		if (!getChildren().isEmpty()) {
+			// remove the node at index 0, the previous screen
 			getChildren().remove(0);
 
-			// get the new screen, add it to the node
-			// TODO screen transitions can be coded here
+			// get the new screen, add it to this StackPane
 			getChildren().add(0, screenMap.get(screenID));
 
-		} else {
+		} else { // this is the first screen so there is nothing to remove
 			getChildren().add(screenMap.get(screenID));
 		}
-
 	}
 
 
-
+	/**
+	 * loads a new WorkoutLibrary screen into the screen map at the same ID as previous WorkoutLibrary
+	 * Call when an updated WorkoutLibrary needs to be put in the <code>screenMap</code> using
+	 * {@link StackPaneUpdater#loadFXMLScreen(String,String) loadFXMLScreen()}
+	 */
 	public void loadWorkoutLibrary() {
 		loadFXMLScreen(Main.workoutLibraryID, Main.workoutPageFile);
 	}
-	
+
+	/**
+	 * loads a new DietMenu screen into the screen map at the same ID as previous DietMenu
+	 * Call when an updated DietMenu needs to be put in the <code>screenMap</code> using
+	 * {@link StackPaneUpdater#loadJavaWrittenScreen(String, Node) loadJavaWrittenScreen}
+	 */
 	public void loadDietMenu() {
 		dietMenu = new DietMenu(screenWidth, screenHeight);
 		loadJavaWrittenScreen(Main.dietMenuID, dietMenu);
 	}
 
+	/**
+	 * loads a new DietPlanner screen into the screen map at the same ID as previous DietPlanner
+	 * Call when an updated DietPlanner needs to be put in the <code>screenMap</code> using
+	 * {@link StackPaneUpdater#loadJavaWrittenScreen(String, Node) loadJavaWrittenScreen}
+	 */
 	public void loadDietPlanner() {
 		dietPlanner = new DietPlanner(screenWidth, screenHeight);
 		dietPlanner.addButtons();
 		loadJavaWrittenScreen(Main.dietPlannerID, dietPlanner);
 
 	}
+
+	/**
+	 * Call at end of presentation
+	 * @param temporaryNode
+     */
 	public void displayNode(Node temporaryNode) {
 		this.getChildren().remove(0);
 		this.getChildren().add(temporaryNode);
 	}
 
+	/**
+	 * creates a new CharacterDashBoard screen into the screen map at the same ID as previous DietPlanner
+	 * Call when an updated DietPlanner needs to be put in the <code>screenMap</code> using
+	 * {@link StackPaneUpdater#loadFXMLScreen(String, String) loadFXMLScreen}
+	 */
 	public void loadCharacterDashboard() {
 		loadFXMLScreen(Main.characterDashID, Main.characterDashFile);
 	}
@@ -182,5 +231,14 @@ public class StackPaneUpdater extends StackPane {
 	public void loadSocialMenu() {
 		socialMenu = new SocialMenu(screenWidth, screenHeight);
 		loadJavaWrittenScreen(Main.socialMenuID, socialMenu);
+	}
+
+	/**
+	 * sets the <code>mainApp</code> reference for this class which will be passed on to screens that implement
+	 *
+	 * @param mainApp
+     */
+	public void setMainApp(Main mainApp) {
+		this.mainApp = mainApp;
 	}
 }
